a9db1260489c109980382084387c358bb16c4b87
scavengerui <ksdhanushreddy@gmail.com>
2026-05-26 16:22:37 +0530

    removed hardcore port numbre

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ app.use(express.json());
 
 app.use("/",urlRoute);
 
-app.listen(8000)
+
+const PORT = process.env.PORT 
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`);
+});
